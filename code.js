e4c015cc3904f6988b332eb2a613341ea25c55bf
Gabe <gehughes28@gmail.com>
2022-09-19 11:50:28 +0200

    Added click to turn grid square black, now need to make drag functionality

--- code.js
+++ code.js
@@ -9,18 +9,26 @@ Add extra grid sizes, colors etc.
 
 //creates grid making variables to then use in CSS with display:grid
 function createGrid (rows, cols) {
-    document.getElementById("container").innerHTML = " "; //resets container div, making them not overlap infinitely
-    let d = document.getElementById("container"); //refers to document; will be used to append divs to  
+    document.getElementById("container").innerHTML = " "; //resets container div, making them not overlap infinitely 
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div");
-        cell.className = "gridsquare";
+        cell.id = 'square';
+        cell.addEventListener('mousedown', function (){ cell.style.backgroundColor = 'black' });
         container.appendChild(cell);
     }
 
 
 }
+
+//experiment
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmousup = () => (mouseDown = false)
+
+
+
 document.getElementById("16-button").addEventListener("click", function (){
     createGrid (16, 16);
 });
@@ -29,21 +37,30 @@ document.getElementById("32-button").addEventListener("click", function (){
     createGrid (32, 32);
 });
 
+// problem is that I need to add activated to the specific gridsquare that is clicked
+
+// document.getElementById("square").addEventListener("mousedown"), function (){
+//     document.getElementById("square").style.backgroundColor = 'black';
+
+// }
+
+    
+//add function to add activated element to gridsquare when mousedown
+
 //created Grid with individual divs for rows and columns
-/*function createGrid (x) {
-    document.getElementById("container").innerHTML = " "; //resets container div, making them not overlap infinitely
-    let d = document.getElementById("container"); //refers to document; will be used to append divs to  
-        for (i = 0; i < x; i++) {
-            //create x number of row divs
-            let row = document.createElement("div");
-            row.className = "row";
-            for (z = 1; z <= x; z++) {
-                let cell = document.createElement("div");
-                cell.className = "gridsquare";
-                // cell.innerText = (i * x) + z
-                row.appendChild(cell);
-            }
-            d.appendChild(row)
-        }
-}
-*/
+// function createGrid (x) {
+//     document.getElementById("container").innerHTML = " "; //resets container div, making them not overlap infinitely
+//     let d = document.getElementById("container"); //refers to document; will be used to append divs to  
+//         for (i = 0; i < x; i++) {
+//             //create x number of row divs
+//             let row = document.createElement("div");
+//             row.className = "row";
+//             for (z = 1; z <= x; z++) {
+//                 let cell = document.createElement("div");
+//                 cell.className = "gridsquare";
+//                 // cell.innerText = (i * x) + z
+//                 row.appendChild(cell);
+//             }
+//             d.appendChild(row)
+//         }
+// }

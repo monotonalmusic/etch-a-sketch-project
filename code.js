9b56946387d1eb7ecdc47838a49aab0c80748178
Gabe <gehughes28@gmail.com>
2022-09-20 16:25:44 +0200

    Cleaned up JS, finally fixed clear functionality using getElementsByTag('*') and .length to read the current size of grid and recreate it upon clear
    Next, need to finally figure out how to add some kind of 'current' class to the color modes

--- code.js
+++ code.js
@@ -15,6 +15,55 @@ window.addEventListener('load', function(){
     createGrid(16,16)
 });
 
+//creates grid with two inputs, rows and columns
+function createGrid (rows, cols) {
+    document.getElementById("container").innerHTML = " "; //resets container div, making them not overlap infinitely 
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    //toggle is enabled if mouse is down
+    container.onmousedown = enableToggle;
+    for (i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div");
+        cell.classList = 'square';
+        //if mouse is down AND mouse enters the div, then proceed to color
+        cell.onmouseenter = toggle;
+        container.appendChild(cell);
+    }
+    //toggle is disabled if mouse is up
+    container.onmouseup = disableToggle;
+
+
+function enableToggle (e) {
+    mouseDown = true;
+
+    if (e.target !== container) {
+        toggle(e)
+    }
+}
+
+function disableToggle () {
+    mouseDown = false
+}
+
+function toggle (e) {
+    if (mouseDown === false) {
+        return
+    }
+    else if (colorMode === 'default') {
+        e.target.style.backgroundColor = 'black' 
+    }
+    else if (colorMode === 'rainbow') {
+        const randomR = Math.floor(Math.random() * 256)
+        const randomG = Math.floor(Math.random() * 256)
+        const randomB = Math.floor(Math.random() * 256)
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})` 
+    }
+    else if (colorMode === 'erase') {
+        e.target.style.backgroundColor = 'beige'
+    }
+}
+
+
 //slider stuff
 let slider = document.getElementById("myRange")
 let output = document.getElementById("slidervalue")
@@ -35,87 +84,29 @@ const eraserbutton = document.getElementById('eraser-button')
 const blackbutton = document.getElementById('black-button')
 const clearbutton = document.getElementById('clear-button')
 
-
+//would like to make 'currentmode' function that will keep the button indented if that colormode is activated
 rainbowbutton.onclick = () => colorMode = 'rainbow'; 
 eraserbutton.onclick = () => colorMode = 'erase'
 blackbutton.onclick = () => colorMode = 'default'
-clearbutton.onclick = () => document.getElementById("container").innerHTML = " "
 
-
-
-
-function enableToggle (e) {
-    mouseDown = true;
-
-    if (e.target !== container) {
-        toggle(e)
-    }
-}
-
-function disableToggle () {
-    mouseDown = false
-}
-
-function toggle (e) {
-    if (mouseDown === false) {
-        return
-    }
-    else if (colorMode === 'default') {
-        e.target.style.backgroundColor = 'black'
-    }
-    else if (colorMode === 'rainbow') {
-        const randomR = Math.floor(Math.random() * 256)
-        const randomG = Math.floor(Math.random() * 256)
-        const randomB = Math.floor(Math.random() * 256)
-        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
-    }
-    else if (colorMode === 'erase') {
-        e.target.style.backgroundColor = 'beige'
-    }
-}
+let currentSize = container.getElementsByTagName('*').length;
+let createNumber = Math.sqrt(currentSize)
+clearbutton.addEventListener ('click', function (){
+    container.innerHTML = " "
+    createGrid(createNumber, createNumber)  
+})
 
 
 
 
 
 
-//creates grid making variables to then use in CSS with display:grid
-function createGrid (rows, cols) {
-    document.getElementById("container").innerHTML = " "; //resets container div, making them not overlap infinitely 
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    //toggle is enabled if mouse is down
-    container.onmousedown = enableToggle;
-    for (i = 0; i < (rows * cols); i++) {
-        let cell = document.createElement("div");
-        cell.classList = 'square';
-        //if mouse is down AND mouse enters the div, then proceed to color
-        cell.onmouseenter = toggle;
-        container.appendChild(cell);
-    }
-    //toggle is disabled if mouse is up
-    container.onmouseup = disableToggle;
 
-}
+
+
+
+
+
 
 
     
-//add function to add activated element to gridsquare when mousedown
-
-//created Grid with individual divs for rows and columns
-// function createGrid (x) {
-//     document.getElementById("container").innerHTML = " "; //resets container div, making them not overlap infinitely
-//     let d = document.getElementById("container"); //refers to document; will be used to append divs to  
-//         for (i = 0; i < x; i++) {
-//             //create x number of row divs
-//             let row = document.createElement("div");
-//             row.className = "row";
-//             for (z = 1; z <= x; z++) {
-//                 let cell = document.createElement("div");
-//                 cell.className = "gridsquare";
-//                 // cell.innerText = (i * x) + z
-//                 row.appendChild(cell);
-//             }
-//             d.appendChild(row)
-//         }
-// }
